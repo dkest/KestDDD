38ff4492d4182e0995f749576dcf67ffdfe1f7d3
DK <ke.ding@foxmail.com>
2020-10-31 14:54:02 +0800

    init

---
+++ ChristDDD.Domain.Core/Commands/Command.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using KestDDD.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KestDDD.Domain.Core.Commands
+{
+    /// <summary>
+    /// 抽象命令基类
+    /// </summary>
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract bool IsValid();
+    }
+}
