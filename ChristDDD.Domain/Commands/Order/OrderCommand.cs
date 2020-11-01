38380223149eb6c4f5a32553640b8144ad1bd8eb
DK <ke.ding@foxmail.com>
2020-11-01 13:51:12 +0800

    Order Model、Command

---
+++ ChristDDD.Domain/Commands/Order/OrderCommand.cs
@@ -0,0 +1,11 @@
+﻿using KestDDD.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KestDDD.Domain.Commands
+{
+    public abstract class OrderCommand : Command
+    {
+    }
+}
